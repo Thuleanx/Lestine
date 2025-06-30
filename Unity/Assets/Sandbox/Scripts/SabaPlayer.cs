06449cf9d2e71abdd20b1f20b9c0ba95e45640e4
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2025-06-29 18:32:15 -0700

    refactor: move player script under saba namespace

--- Unity/Assets/Sandbox/Scripts/SabaPlayer.cs
+++ Unity/Assets/Sandbox/Scripts/SabaPlayer.cs
@@ -1,26 +1,31 @@
 using UnityEngine;
 using Scriptables;
 
-public class SabaPlayer : MonoBehaviour {
-    [SerializeField] ScriptableVector2 movementInput;
-    [SerializeField] Camera mainCamera;
-    [SerializeField] float speed;
+namespace Saba {
+	public class SabaPlayer : MonoBehaviour {
+		[SerializeField]
+		ScriptableVector2 movementInput;
+		[SerializeField]
+		Camera mainCamera;
+		[SerializeField]
+		float speed;
 
-    void Awake() {
-        if (!mainCamera)
-            mainCamera = Camera.main;
-    }
+		void Awake() {
+			if (!mainCamera) mainCamera = Camera.main;
+		}
 
-    void Update() {
-        Vector3 right = mainCamera.transform.right;
-        Vector3 forward = mainCamera.transform.forward;
-        right.y = 0;
-        forward.y = 0;
-        right = right.normalized;
-        forward = forward.normalized;
+		void Update() {
+			Vector3 right = mainCamera.transform.right;
+			Vector3 forward = mainCamera.transform.forward;
+			right.y = 0;
+			forward.y = 0;
+			right = right.normalized;
+			forward = forward.normalized;
 
-        Vector3 desiredMoveDirection = right * movementInput.Value.x + forward * movementInput.Value.y;
+			Vector3 desiredMoveDirection =
+				right * movementInput.Value.x + forward * movementInput.Value.y;
 
-        transform.position += speed * Time.deltaTime * desiredMoveDirection;
-    }
+			transform.position += speed * Time.deltaTime * desiredMoveDirection;
+		}
+	}
 }
